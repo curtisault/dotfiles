867dfad745f01e1233ac4208e5fb75a95a8d6067
Curtis Ault <curtisault@Curtiss-MacBook-Pro-2.local>
2026-02-17 13:48:47 -0600

    fish: bao completions

---
+++ macos/fish/.config/fish/completions/bao.fish
@@ -0,0 +1,7 @@
+function __complete_bao
+    set -lx COMP_LINE (commandline -cp)
+    test -z (commandline -ct)
+    and set COMP_LINE "$COMP_LINE "
+    /opt/homebrew/bin/bao
+end
+complete -f -c bao -a "(__complete_bao)"
