45efe698ca91854240c1f5f5e4d4ff4fcfabb511
curtisault <curtisault@gmail.com>
2026-04-10 15:49:05 -0500

    add lmstudio

--- macos/fish/.config/fish/conf.d/10-paths.fish
+++ macos/fish/.config/fish/conf.d/10-paths.fish
@@ -4,4 +4,5 @@ fish_add_path /opt/homebrew/sbin
 fish_add_path $HOME/.cargo/bin
 fish_add_path /opt/homebrew/opt/postgresql@17/bin
 fish_add_path $HOME/.local/bin
+fish_add_path $HOME/.lmstudio/bin
 
